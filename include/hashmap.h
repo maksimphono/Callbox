95956c9480f378c142b7a40043dce82ca0a1e7a0
maksimphono <trukhinmaksimof@mail.ru>
2026-06-17 23:42:40 +0800

    feat: implemented method "drop" to delete items from hashmap

--- include/hashmap.h
+++ include/hashmap.h
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <stdio.h>
 
 #include "defs.h"
 
@@ -29,14 +30,14 @@
         Val_T _val; \
         struct Name##_entry_t* next; \
     } Name##_entry_t; \
-    typedef struct Name##_hashmap_t { \
+    typedef struct Name { \
         Name##_entry_t** entries; \
         size_t size; \
-    } Name##_hashmap_t;\
-    const Val_T Name##_hashmap_NOT_FOUND = (const Val_T)Not_found_V; \
+    } Name;\
+    const Val_T Name##_NOT_FOUND = (const Val_T)Not_found_V; \
 \
-Name##_hashmap_t* new_##Name##_hashmap(size_t size) { \
-    Name##_hashmap_t* new_hashmap_instance = (Name##_hashmap_t*)malloc(sizeof(Name##_hashmap_t)); \
+Name* new_##Name(size_t size) { \
+    Name* new_hashmap_instance = (Name*)malloc(sizeof(Name)); \
     new_hashmap_instance->size = size; \
     new_hashmap_instance->entries = (Name##_entry_t**)malloc(size * sizeof(Name##_entry_t*)); \
 \
@@ -62,7 +63,7 @@ void del_##Name##_entry(Name##_entry_t* this) {  \
 \
     free(this); \
 }\
-void del_##Name##_hashmap(Name##_hashmap_t* this) { \
+void del_##Name(Name* this) { \
     if (this == 0x0) return; \
 \
     Name##_entry_t* current = 0x0, *next = 0x0;\
@@ -79,7 +80,7 @@ void del_##Name##_hashmap(Name##_hashmap_t* this) { \
     free(this->entries); \
     free(this); \
 } \
-void insert_##Name##_hashmap(Name##_hashmap_t* this, Key_T key, Val_T val) { \
+void insert_##Name(Name* this, Key_T key, Val_T val) { \
     if (this == 0x0) return; \
     size_t id = Hash_F(key) % this->size; \
 \
@@ -93,23 +94,48 @@ void insert_##Name##_hashmap(Name##_hashmap_t* this, Key_T key, Val_T val) { \
         this->entries[id] = new_##Name##_entry(key, val); \
     } \
 } \
-Val_T get_##Name##_hashmap(Name##_hashmap_t* this, Key_T key) { \
+Val_T get_##Name(Name* this, Key_T key) { \
     if (this == 0x0) return (Val_T)0x0; \
 \
     size_t id = Hash_F(key) % this->size; \
 \
     Name##_entry_t* current = this->entries[id]; \
 \
-    while (current != 0x0 && Cmp_F(current->_key, key) == 0) { \
+    while (current != 0x0 && !Cmp_F(current->_key, key)) { \
         current = current->next; \
     } \
 \
     if (current != 0x0) { \
         return current->_val; \
     } else { \
-        return Name##_hashmap_NOT_FOUND; \
+        return Name##_NOT_FOUND; \
     } \
 } \
+void drop_##Name(Name* this, Key_T key) { \
+    if (this == 0x0) return; \
+\
+    size_t id = Hash_F(key) % this->size; \
+\
+    Name##_entry_t* current = this->entries[id]; \
+    Name##_entry_t* prev = this->entries[id]; \
+\
+    while (current != 0x0 && !Cmp_F(current->_key, key)) { \
+        prev = current; \
+        current = current->next; \
+    } \
+\
+    if (current == 0x0) { \
+        return; \
+    } \
+\
+    if (current == this->entries[id]) { \
+        this->entries[id] = current->next; \
+        del_##Name##_entry(current); \
+    } else { \
+        prev->next = current->next; \
+        del_##Name##_entry(current); \
+    } \
+}\
 
 #define EMPTY_F(...)
 
@@ -129,17 +155,54 @@ size_t hash(char *str) {
 
 #define STR_CPY(__dest, __src) __dest = strdup(__src)
 #define ASSIGN(__dest, __src) __dest = __src
+#define SIMPLE_EQ(__a, __b) (__a == __b)
 #define STR_CMP(__a, __b) (strcmp(__a, __b) == 0)
 
-DEFINE_HASHMAP(str_int, char*, int, STR_CPY, ASSIGN, STR_CMP, hash,free,EMPTY_F, -99)
+size_t hash_int(int n) {
+    return n;
+}
+
+DEFINE_HASHMAP(str_int_hashmap_t, char*, int, STR_CPY, ASSIGN, STR_CMP, hash,free,EMPTY_F, -99)
+DEFINE_HASHMAP(int_char_HM, int, char, ASSIGN, ASSIGN, SIMPLE_EQ, hash_int,EMPTY_F,EMPTY_F, '@');
 
 int main() {
-    str_int_hashmap_t* hm = new_str_int_hashmap(2);
-    insert_str_int_hashmap(hm, "one", 1);
-    insert_str_int_hashmap(hm, "two", 2);
-    insert_str_int_hashmap(hm, "tree", 3);
+    str_int_hashmap_t* hm = new_str_int_hashmap_t(2);
+    int_char_HM* int_chat = new_int_char_HM(4);
+    
+    for (int i = 0; i < 14; i++) {
+        insert_int_char_HM(int_chat, i, i + 97);
+    }
 
-    del_str_int_hashmap(hm);
+    drop_int_char_HM(int_chat, 10); // 
+    drop_int_char_HM(int_chat, 6);
+    for (int i = 14; i < 20; i++) {
+        insert_int_char_HM(int_chat, i, i + 97);
+    }
+    drop_int_char_HM(int_chat, 3);
+    //drop_int_char_HM(int_chat, 667);
+    //drop_int_char_HM(int_chat, 665);
+    //drop_int_char_HM(int_chat, -1);
+    drop_int_char_HM(int_chat, 13);
+
+    for (int i = 0; i < 20; i++) {
+        char c = get_int_char_HM(int_chat, i);
+        if (c != '@') {
+            printf("%d : %c\n", i, c);
+        }
+    }    
+
+
+    insert_str_int_hashmap_t(hm, "one", 1);
+    insert_str_int_hashmap_t(hm, "two", 2);
+    insert_str_int_hashmap_t(hm, "tree", 3);
+
+    drop_str_int_hashmap_t(hm, "three");
+    drop_str_int_hashmap_t(hm, "two");
+    drop_str_int_hashmap_t(hm, "t");
+    drop_str_int_hashmap_t(hm, "one");
+
+    del_str_int_hashmap_t(hm);
+    del_int_char_HM(int_chat);
     return 0;
 }
 
